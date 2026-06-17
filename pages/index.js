acb2b449e4732253a2fc35d9445a2224a034523e
Xiomara-SOcontrol <xiomara.bellondonorio@set.ypf.com>
2026-06-17 14:42:08 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Sistema de Salud Ocupacional</h1>
+
+      <div style={{ marginTop: 20 }}>
+        <div>🩺 Atención Médica</div>
+        <div>💉 Enfermería</div>
+        <div>📋 Exámenes Periódicos</div>
+        <div>🏭 EMP</div>
+      </div>
+    </div>
+  );
+}
